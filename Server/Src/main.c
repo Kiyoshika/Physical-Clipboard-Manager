17824175d81f68d60c8ca085603512a08b2def12
kiyoshika <zachlweaver00@gmail.com>
2022-04-19 19:52:18 -0400

    now pastes the content from clipboard + escape routine
    now added listener to ESCAPE key to properly close the connection. when pressing paste button, now using win API to really paste the content from the clipboard fetched by MCU

--- Server/Src/main.c
+++ Server/Src/main.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <winuser.h>
 #include <stdio.h>
 #include <time.h>
 #include "clipboard.h"
@@ -22,14 +23,6 @@ int main(int argc, char* argv[])
                                 OPEN_EXISTING,
                                 0,
                                 NULL);
-    
-    // without these timeouts, reading data is extremely slow
-    COMMTIMEOUTS timeouts;
-    timeouts.ReadIntervalTimeout = 0;
-    timeouts.ReadTotalTimeoutMultiplier = 0;
-    timeouts.ReadTotalTimeoutConstant = 1;
-    timeouts.WriteTotalTimeoutMultiplier = 0;
-    timeouts.WriteTotalTimeoutConstant = 0;
 
     SetCommTimeouts(hComm, &timeouts);
 
@@ -39,7 +32,15 @@ int main(int argc, char* argv[])
         exit(-1);
     }
     else
-        printf("Now listening for client on port %s (press Ctrl+C anytime to kill server.)\n", argv[1]);
+        printf("Now listening for client on port %s (press ESC anytime to kill server and close connection)\n", argv[1]);
+
+    // without these timeouts, reading data is extremely slow
+    COMMTIMEOUTS timeouts;
+    timeouts.ReadIntervalTimeout = 0;
+    timeouts.ReadTotalTimeoutMultiplier = 0;
+    timeouts.ReadTotalTimeoutConstant = 1;
+    timeouts.WriteTotalTimeoutMultiplier = 0;
+    timeouts.WriteTotalTimeoutConstant = 0;
 
     DWORD dwBytesRead, dwBytesWritten;
     char data[255];
@@ -47,9 +48,20 @@ int main(int argc, char* argv[])
     char client_send_data[261] = {0}; // prepend command such as "COPY1datatosend"
     BOOL received;
 
+    // set up keyboard to make keypresses (specifically CTRL+V)
+    INPUT keyboard_output;
+    keyboard_output.type = INPUT_KEYBOARD;
+    keyboard_output.ki.wScan = 0; // hardware scan code for key
+    keyboard_output.ki.time = 0;
+    keyboard_output.ki.dwExtraInfo = 0;
+
     
     while (1)
     {
+        // if ESC is pressed, break out of for loop and close serial connection
+        if (GetKeyState(VK_ESCAPE) & 0x8000)
+            break;
+
         received = ReadFile(hComm, &data, 255, &dwBytesRead, NULL);
         if (dwBytesRead > 0)
         {
@@ -71,7 +83,20 @@ int main(int argc, char* argv[])
                 printf("Trying to paste: %s\n", data);
                 set_clipboard_content(data);
                 memset(data, 0, strlen(data));
-                // TODO: perform an actual paste operation using WinAPI
+
+                keyboard_output.ki.wVk = VK_LCONTROL;
+                keyboard_output.ki.dwFlags = 0;
+                SendInput(1, &keyboard_output, sizeof(INPUT));
+
+                keyboard_output.ki.wVk = 0x56; // V key
+                keyboard_output.ki.dwFlags = 0;
+                SendInput(1, &keyboard_output, sizeof(INPUT));
+
+                keyboard_output.ki.dwFlags = KEYEVENTF_KEYUP; // release V key
+                SendInput(1, &keyboard_output, sizeof(INPUT));
+
+                keyboard_output.ki.wVk = VK_LCONTROL; // release left control key
+                SendInput(1, &keyboard_output, sizeof(INPUT));
             }
         }
     }
@@ -79,4 +104,3 @@ int main(int argc, char* argv[])
     CloseHandle(hComm);
     return 0;
 }
-
